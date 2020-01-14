6040047c5881295dfca12f3f08bce67df6b53a25
Ali Kalkanel <Kally-95@hotmail.com>
2020-01-14 21:05:20 +0000

    retrieve funds button working

--- client/src/App.js
+++ client/src/App.js
@@ -138,7 +138,7 @@ class App extends Component {
     let { loanId, contract, accounts } = this.state;
     await contract.methods
     .retrieveLoanFunds(loanId)
-    .call({from: accounts[0]})
+    .send({from: accounts[0]})
   };
 
   handleRetrieveLoans = async () => {
@@ -154,12 +154,6 @@ class App extends Component {
     console.log(event.target.name)
     console.log(event.target.value)
     this.setState({ [event.target.name]: event.target.value });
-  };
-
-  handleInterest = () => {
-    const { accounts, contract } = this.state;
-    const { interest } = this.state
-    console.log(this.state.interest);
   };
 
   render() {
