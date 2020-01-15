4c4a94e94007dfef2f1944fb37e22e1d3c80f419
Ali Kalkanel <Kally-95@hotmail.com>
2020-01-15 15:25:16 +0000

    Added more styling to header

--- client/src/App.js
+++ client/src/App.js
@@ -174,8 +174,9 @@ class App extends Component {
       <div className="App">
         <div className="Loan-app-header">
         <h1>LOANR</h1>
+        <img className="header-img" src="https://cdn.iconscout.com/icon/free/png-256/ethereum-3-569581.png"/>
         <p class="line-1 anim-typewriter">Built on Ethereum smart contracts</p>
-        <span>Account: {this.state.accounts ? this.state.accounts : null }</span>
+        <span className="account-address">Account: {this.state.accounts ? this.state.accounts : null }</span>
         </div>
      
         <div>This Loan has an ID of: {this.state.loanId}</div>
